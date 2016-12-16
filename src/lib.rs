9be6f51ce8da9c79e3c9cd0dafac0857bd7aaf04
Jos van den Oever <jos@vandenoever.info>
2016-12-16 17:44:42 +0100

    Implement rdfliteral

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,136 @@
-mod turtle;
-use turtle::*;
 #[macro_use]
 extern crate nom;
 use nom::*;
 use nom::IResult::*;
 
-pub fn parse_string_with_lalrpop(data: &str) {
-    let r = parse_LangTag(data);
-    println!("{:?}", r);
+fn is_alpha(a: char) -> bool {
+  (a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z')
 }
 
-pub fn run() {
-    let data = "@nl";
-    parse_string_with_lalrpop(data);
+fn is_alphanum(a: char) -> bool {
+  (a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z') || (a >= '0' && a <= '9')
 }
 
-#[test]
-fn test_langtag() {
-    assert_eq!(parse_LangTag("@nl").unwrap(), "nl");
-    assert!(parse_LangTag("@nl-NL").is_ok());
-}
-
-named!(alpha, take_while!(is_alphabetic));
-named!(alphanumeric, take_while!(is_alphanumeric));
-
-named!(langtag,
-       do_parse!(tag!("@") >> langtag: alpha >> opt!(langtag_suffix) >> (langtag)));
+named!(alpha<&str,&str>, take_while!(is_alpha));
+named!(alphanumeric<&str,&str>, take_while!(is_alphanum));
 
 #[inline]
-fn is_string_literal_quote(chr: u8) -> bool {
-    chr != 0x22 && chr != 0x5C && chr != 0xA && chr != 0xD
+fn is_string_literal_quote(chr: char) -> bool {
+    chr != '"' && chr != '\\' && chr != '\n' && chr != '\r'
 }
 
-named!(string_literal_quote,
-       delimited!(tag!("\""), take_while!(is_string_literal_quote), tag!("\"")));
-
-named!(langtag_suffix,
-       do_parse!(tag!("-") >> langtag: alphanumeric >> (langtag)));
-
-// named!(parens, delimited!(char!('('), is_not!(")"), char!(')')));
-named!(parens, ws!(delimited!(tag!("("), is_not!(")"), tag!(")"))));
+named!(string_literal_quote<&str,String>,
+  delimited!(
+    tag!("\""),
+    map!(
+      take_while!(is_string_literal_quote),
+      String::from),
+    tag!("\"")
+  )
+);
 
 #[test]
 fn test_string_literal_quote() {
-
-    assert_eq!(string_literal_quote(b"\"\""), Done(&b""[..], &b""[..]));
+    assert_eq!(string_literal_quote("\"\""), Done(&""[..], String::from("")));
 }
 
+#[inline]
+fn is_string_literal_single_quote(chr: char) -> bool {
+    chr != '\'' && chr != '\\' && chr != '\n' && chr != '\r'
+}
+
+named!(string_literal_single_quote<&str,String>,
+  delimited!(
+    tag!("'"),
+    map!(
+      take_while!(is_string_literal_single_quote),
+      String::from),
+    tag!("'")
+  )
+);
+
 #[test]
-fn test_langtag_nom() {
-    assert_eq!(langtag(b"@nl "), Done(&b" "[..], &b"nl"[..]));
-    assert_eq!(parens(b"(hi)"), Done(&b""[..], &b"hi"[..]));
-    assert_eq!(parens(b" (hi) "), Done(&b""[..], &b"hi"[..]));
+fn test_string_literal_single_quote() {
+    assert_eq!(string_literal_single_quote("''"), Done(&""[..], String::from("")));
+}
+
+#[inline]
+fn is_string_literal_long_single_quote(chr: char) -> bool {
+    chr != '\'' as char && chr != '\\' && chr != '\n' && chr != '\r'
+}
+
+named!(string_literal_long_single_quote<&str,String>,
+  delimited!(
+    tag!("'''"),
+    map!(
+      take_while!(is_string_literal_long_single_quote),
+      String::from),
+    tag!("'''")
+  )
+);
+
+
+#[test]
+fn test_string_literal_long_single_quote() {
+    assert_eq!(string_literal_long_single_quote("''''''"), Done(&""[..], String::from("")));
+}
+
+#[inline]
+fn is_string_literal_long_quote(chr: char) -> bool {
+    chr != '"' && chr != '\\' && chr != '\n' && chr != '\r'
+}
+
+named!(string_literal_long_quote<&str,String>,
+  delimited!(
+    tag!("\"\"\""),
+    map!(
+      take_while!(is_string_literal_long_quote),
+      String::from),
+    tag!("\"\"\"")
+  )
+);
+
+#[test]
+fn test_string_literal_long_quote() {
+    assert_eq!(string_literal_long_quote("\"\"\"\"\"\""), Done(&""[..], String::from("")));
+}
+
+named!(string<&str,String>, alt!(string_literal_quote | string_literal_single_quote
+    | string_literal_long_single_quote | string_literal_long_quote));
+
+named!(langtag<&str,String>, do_parse!(
+  tag!("@") >>
+  langtag: alpha >>
+  opt!(langtag_suffix) >>
+  (String::from(langtag))
+));
+
+named!(langtag_suffix<&str,&str>,
+       do_parse!(tag!("-") >> langtag: alphanumeric >> (langtag)));
+
+#[test]
+fn test_langtag() {
+    assert_eq!(langtag("@nl "), Done(&" "[..], String::from("nl")));
+}
+
+#[derive(Debug,PartialEq,Eq)]
+struct RDFLiteral {
+  string: String,
+  langtag: Option<String>
+}
+
+named!(rdfliteral<&str,RDFLiteral>, do_parse!(
+  string: string >>
+  langtag: opt!(langtag) >>
+  (RDFLiteral{string:string,langtag:langtag})
+));
+
+#[test]
+fn test_rdfliteral() {
+    let r = RDFLiteral{string:String::from(""),langtag:None};
+    assert_eq!(rdfliteral("'' "), Done(&" "[..], r));
+}
+
+pub fn parse() {
+    rdfliteral("''");
 }
