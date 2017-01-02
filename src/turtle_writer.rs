ac57b0551609fb7a6fb9a7d5273df079ecdc1d13
Jos van den Oever <jos@vandenoever.info>
2017-01-02 22:17:48 +0100

    Refactor towards making sruct Namespaces independent

--- src/turtle_writer.rs
+++ src/turtle_writer.rs
@@ -2,6 +2,7 @@ use std::io::{Result, Write};
 use graph::*;
 use grammar;
 
+#[derive (Clone)]
 pub struct Namespaces {
     namespaces: Vec<Namespace>,
 }
@@ -16,8 +17,17 @@ impl Namespaces {
             prefix: Vec::from(prefix),
         });
     }
+    fn find_prefix<'a>(&self, iri: &'a str) -> Option<(&[u8], &'a str)> {
+        for ns in self.namespaces.iter() {
+            if iri.starts_with(&ns.namespace) {
+                return Some((ns.prefix.as_slice(), &iri[ns.namespace.len()..]));
+            }
+        }
+        None
+    }
 }
 
+#[derive (Clone)]
 struct Namespace {
     namespace: String,
     prefix: Vec<u8>,
@@ -28,7 +38,6 @@ struct TurtleWriter<'a, W>
 {
     buffer: Vec<u8>,
     base: String,
-    namespaces: &'a Vec<Namespace>,
     writer: &'a mut W,
     last_subject: SubjectClone,
     open_statement: bool,
@@ -42,30 +51,20 @@ pub fn write_turtle<T, I, W>(namespaces: &Namespaces, triples: I, writer: &mut W
     let mut writer = TurtleWriter {
         buffer: Vec::new(),
         base: String::new(),
-        namespaces: &namespaces.namespaces,
         writer: writer,
         last_subject: SubjectClone::new(),
         open_statement: false,
     };
-    for ns in writer.namespaces {
-        try!(writer.write_prefix(ns));
+    for ns in namespaces.namespaces.iter() {
+        try!(writer.write_prefix(&ns));
     }
     try!(writer.writer.write_all(b"\n"));
     for triple in triples {
-        try!(writer.write_triple(&triple));
+        try!(writer.write_triple(&triple, namespaces));
     }
     writer.writer.write_all(b" .\n")
 }
 
-fn find_prefix<'a>(iri: &'a str, namespaces: &Vec<Namespace>) -> Option<(&'a str, usize)> {
-    for i in 0..namespaces.len() {
-        let ns = namespaces[i].namespace.as_str();
-        if iri.starts_with(ns) {
-            return Some((&iri[ns.len()..], i));
-        }
-    }
-    None
-}
 
 impl<'a, W> TurtleWriter<'a, W>
     where W: Write + 'a
@@ -77,19 +76,18 @@ impl<'a, W> TurtleWriter<'a, W>
         try!(self.write_full_iri(ns.namespace.as_str()));
         self.writer.write_all(b" .\n")
     }
-
-    fn write_iri(&mut self, iri: &str) -> Result<()> {
+    fn write_iri(&mut self, iri: &str, namespaces: &Namespaces) -> Result<()> {
         if iri == grammar::RDF_TYPE {
             self.writer.write_all(b"a")
         } else {
-            match find_prefix(iri, &self.namespaces) {
-                Some((iri, prefix)) => self.write_prefixed_iri(iri, prefix),
+            match namespaces.find_prefix(iri) {
+                Some((prefix, iri)) => self.write_prefixed_iri(prefix, iri),
                 None => self.write_full_iri(iri),
             }
         }
     }
-    fn write_prefixed_iri(&mut self, iri: &str, prefix: usize) -> Result<()> {
-        try!(self.writer.write_all(self.namespaces[prefix].prefix.as_slice()));
+    fn write_prefixed_iri(&mut self, prefix: &[u8], iri: &str) -> Result<()> {
+        try!(self.writer.write_all(prefix));
         try!(self.writer.write_all(b":"));
         self.writer.write_all(iri.as_bytes())
     }
@@ -124,7 +122,7 @@ impl<'a, W> TurtleWriter<'a, W>
         }
         self.writer.write_all(&self.buffer[..])
     }
-    fn write_literal(&mut self, literal: &Literal) -> Result<()> {
+    fn write_literal(&mut self, literal: &Literal, namespaces: &Namespaces) -> Result<()> {
         try!(self.writer.write_all(b"\""));
         try!(self.write_literal_value(&literal.lexical));
         try!(self.writer.write_all(b"\""));
@@ -133,28 +131,28 @@ impl<'a, W> TurtleWriter<'a, W>
             try!(self.writer.write_all(langtag.as_bytes()));
         } else if literal.datatype != "http://www.w3.org/2001/XMLSchema#string" {
             try!(self.writer.write_all(b"^^"));
-            try!(self.write_iri(&literal.datatype));
+            try!(self.write_iri(&literal.datatype, namespaces));
         }
         Ok(())
     }
 
-    fn write_subject(&mut self, subject: &Subject) -> Result<()> {
+    fn write_subject(&mut self, subject: &Subject, namespaces: &Namespaces) -> Result<()> {
         match subject {
-            &Subject::IRI(ref iri) => self.write_iri(&iri),
+            &Subject::IRI(ref iri) => self.write_iri(&iri, namespaces),
             &Subject::BlankNode(blank_node) => self.write_blank_node(&blank_node),
         }
     }
-    fn write_predicate(&mut self, predicate: &str) -> Result<()> {
-        self.write_iri(predicate)
+    fn write_predicate(&mut self, predicate: &str, namespaces: &Namespaces) -> Result<()> {
+        self.write_iri(predicate, namespaces)
     }
-    fn write_object(&mut self, object: &Object) -> Result<()> {
+    fn write_object(&mut self, object: &Object, namespaces: &Namespaces) -> Result<()> {
         match object {
-            &Object::IRI(ref iri) => self.write_iri(&iri),
+            &Object::IRI(ref iri) => self.write_iri(&iri, namespaces),
             &Object::BlankNode(blank_node) => self.write_blank_node(&blank_node),
-            &Object::Literal(ref literal) => self.write_literal(&literal),
+            &Object::Literal(ref literal) => self.write_literal(&literal, namespaces),
         }
     }
-    fn write_triple<T>(&mut self, triple: &T) -> Result<()>
+    fn write_triple<T>(&mut self, triple: &T, namespaces: &Namespaces) -> Result<()>
         where T: Triple
     {
         let subject = triple.subject();
@@ -164,13 +162,13 @@ impl<'a, W> TurtleWriter<'a, W>
             if self.open_statement {
                 try!(self.writer.write_all(b" .\n"));
             }
-            try!(self.write_subject(&triple.subject()));
+            try!(self.write_subject(&triple.subject(), namespaces));
             self.last_subject.assign(&subject);
             try!(self.writer.write_all(b" "));
         }
         self.open_statement = true;
-        try!(self.write_predicate(&triple.predicate()));
+        try!(self.write_predicate(&triple.predicate(), namespaces));
         try!(self.writer.write_all(b" "));
-        self.write_object(&triple.object())
+        self.write_object(&triple.object(), namespaces)
     }
 }
